82ea0419c904b898720baf2702532692c8bdd9b6
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-24 06:06:42 -0300

    feat: reforçando os conceitos de Stack e Heap em memória.

--- alura-memoria-Aula-1/UsuarioLib/Program.cs
+++ alura-memoria-Aula-1/UsuarioLib/Program.cs
@@ -1,4 +1,15 @@
-﻿using UsuarioLib;
+﻿/*                                          Anotações
+    
+    Stack (Pilha de Dados) -> onde temos valores armazenados do type int, bool, double, byte
+    
+    Heap -> (Objetos / Tipos mais complexos)
+
+    LOH -> (É uma Heap para objetos mutio grandes) >= 85kb
+    
+ */
+
+
+using UsuarioLib;
 
 Usuario usuarioNew =
     new Usuario(
@@ -6,6 +17,12 @@ Usuario usuarioNew =
         "gustavo@gmail.com",
         new List<string>() { "12345678" });
 
+
+Usuario usuarioNew2 =
+    new Usuario(
+        "Rafael",
+        "rafael@gmail.com",
+        new List<string>() { "44245678" });
 
 //12345678
 usuarioNew.ExibeTelefones();
